30546e4c2727e7d1b60b626e985ff4da6e7c0a68
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-05-26 16:39:30 -0300

    style: ajust no icon nano e add responsividade

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Nano from "../components/nano_icon";
 import Header from '../components/Header/index';
 import Render from "./renderContent";
+import Link from "next/link";
 
 export default function Home() {
 
@@ -9,9 +10,10 @@ export default function Home() {
     <>
 
     <Header.base>
-      <h1 className="text-center text-4xl font-thin text-zinc-200">nano</h1>
-
-      <Nano.Skew/>
+    <h1 className="text-center text-4xl font-thin text-zinc-200 sm:hidden block">nano</h1>
+      <Link href={'/'}>
+        <Nano.Skew/>
+      </Link>
     </Header.base>   
     
     <main className="lg:max-w-[51rem] md:max-w-[32rem] max-w-96 min-h-screen h-full text-neutral-50 m-auto py-14 ">
--- web/src/components/Header/base.tsx
+++ web/src/components/Header/base.tsx
@@ -2,7 +2,7 @@ import { Children } from "@/Types/interfaces"
 
 export const HeaderBase = ({children}: Children) => {
     return (
-    <header className=" flex m-auto max-w-96 items-center py-10 gap-5 justify-center" >
+    <header className=" flex m-auto max-w-96 items-center py-12 gap-5 justify-center" >
 
       
       <div className="max-w-96 inline-flex gap-10 justify-center items-center">
