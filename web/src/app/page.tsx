53e2423e82c7d8901eea74068bc824234d876b88
Isaac S. Silva <85599926+IsaacGSS@users.noreply.github.com>
2024-05-27 08:27:05 +0000

    update background color main page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -7,21 +7,21 @@ export default function Home() {
 
   return (
 
-    <>
-
+    <main className="bg-zinc-950 w-full min-h-screen">
     <Header.base>
-    <h1 className="text-center text-4xl font-thin text-zinc-200 sm:hidden block">nano</h1>
-      <Link href={'/'}>
-        <Nano.N/>
-      </Link>
-    </Header.base>   
-    
-    <main className="lg:max-w-[51rem] md:max-w-[32rem] max-w-96 min-h-full text-neutral-50 m-auto py-14 ">
+      <h1 className="text-center text-4xl font-thin text-zinc-200 sm:hidden block">nano</h1>
+        <Link href={'/'}>
+          <Nano.N/>
+        </Link>
+      </Header.base>
+        
+      
+      <section className="lg:max-w-[51rem] md:max-w-[32rem] max-w-96 min-h-full text-neutral-50 m-auto py-14 ">
 
-      <Render />
+        <Render />
 
+      </section>
     </main>
-    </>
 
     
 
