72966f81c617edf17da6418fbd79e41732bb2998
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-05-29 03:18:21 -0300

    Update index.ts

--- web/src/components/nano_icon/index.ts
+++ web/src/components/nano_icon/index.ts
@@ -1,11 +1,11 @@
-import { NanoSkew, NanoSkewJs, NanoSkewTs } from "./nano-dublue-skew";
+import { NanoSkew } from "./nano-dublue-skew";
 import { NanoMIn } from "./nano-min";
 
 const IconNano = {
     Min: NanoMIn,
     N: NanoSkew,
-    Js: NanoSkewJs,
-    Ts: NanoSkewTs
+    // Js: NanoSkewJs,
+    // Ts: NanoSkewTs
 }
 
 export default IconNano
