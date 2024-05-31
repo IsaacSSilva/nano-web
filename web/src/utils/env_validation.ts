02f102d259229faa0b76141e2d91b1cb5007dbee
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-05-31 09:53:55 -0300

    Update env_validation.ts

--- web/src/utils/env_validation.ts
+++ web/src/utils/env_validation.ts
@@ -2,6 +2,7 @@ export const url_api = {
     mensagens: process.env.URL_KEY_API ? (process.env.URL_KEY_API + 'mensagens/') : "http://localhost:3333/mensagens/",
     root: process.env.URL_KEY_API ? process.env.URL_KEY_API : "http://localhost:3333/",
     adm: process.env.URL_KEY_API ? (process.env.URL_KEY_API + 'user/adm') : "http://localhost:3333/user/adm",
+    topics: process.env.URL_KEY_API ? (process.env.URL_KEY_API + 'topics') : "http://localhost:3333/topics",
 } 
 
 export const time_revalidating = process.env.TIME_REVALIDATING ? Number(process.env.TIME_REVALIDATING) : 30
