0cbb8e89a817508caec03c8b35177d68f279ec16
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-05-31 09:53:35 -0300

    Update get.ts

--- web/src/app/api/http/get.ts
+++ web/src/app/api/http/get.ts
@@ -1,6 +1,6 @@
 import { time_revalidating, url_api } from "@/utils/env_validation";
 
-async function getData() {
+async function getMensagens() {
     const res = await fetch(url_api.mensagens, { next: { revalidate: time_revalidating } })
    
     if (!res.ok) {
@@ -8,6 +8,32 @@ async function getData() {
     }
 
     return res.json()
+}
+
+async function getTopics() {
+    const res = await fetch(url_api.topics, { next: { revalidate: time_revalidating } })
+   
+    if (!res.ok) {
+    throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+}
+
+async function getAdimin() {
+    const res = await fetch(url_api.adm, { next: { revalidate: time_revalidating } })
+   
+    if (!res.ok) {
+    throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+}
+
+const getData = {
+    mensagens: getMensagens,
+    topics: getTopics,
+    adm: getAdimin
 }
 
 export default getData
